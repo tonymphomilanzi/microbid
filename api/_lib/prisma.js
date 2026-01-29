506ebb621f86d8432a11b60bcb1800299f6de3ae
Tony Milanzi <113247305+tonymphomilanzi@users.noreply.github.com>
2026-01-29 15:47:34 +0200

    Refactor PrismaClient import and initialization

--- api/_lib/prisma.js
+++ api/_lib/prisma.js
@@ -1,8 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+
+const { PrismaClient } = pkg;
 
 const globalForPrisma = globalThis;
 
 export const prisma =
-  globalForPrisma.__prisma ?? new PrismaClient({ log: ["error", "warn"] });
+  globalForPrisma.__prisma ??
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.__prisma = prisma;
